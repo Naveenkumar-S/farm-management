64a5f716ea922f4609b36efe1800f62e7bf84d31
Naveen <naveen.k01@samsung.com>
2025-03-25 08:36:18 +0530

    initial commit

--- schema/user-routes-schema.js
+++ schema/user-routes-schema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi'),
   _ = require('lodash'),
   Enum = require('./../common/enum'),
-  Constants = require('../common/Constants');
+  Constants = require('../common/constants');
 
 const SignUp = Joi.object({
   first_name: Joi.string().min(2).max(30).required(),
